75f382825fffc60c6c0cb44175bcdd862aa2d2f9
dimitris <iracleous@gmail.com>
2021-10-15 09:05:38 +0300

    Add project files.

---
+++ NbgMovies/Model/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NbgMovies.Model
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Movie> Movies { get; set; }
+
+    }
+
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Actor> Actors { get; set; }
+        public Genre Genre { get; set; }
+    }
+
+    public enum Genre
+    {
+        ACTION, COMEDY, DOCUMENTARY, DRAMA, FINCTION, SCIFI
+    }
+
+}
---
+++ NbgMovies/Model/MovieDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NbgMovies.Model
+{
+    public class MovieDbContext:DbContext
+    {
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=NbgMovies;Integrated Security=True");
+            }
+
+        }
+
+
+    }
+}
